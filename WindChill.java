2b0818d4bc8113d480709ee9d7665b368b0d4034
moshtab <mdmohsin41439@gmail.com>
2021-07-31 22:50:08 +0530

    modified by giving method

--- WindChill.java
+++ WindChill.java
@@ -8,20 +8,23 @@ public class WindChill {
 		Scanner s = new Scanner(System.in);
 		System.out.println("enter temperature : ");
 		double t = s.nextDouble();
-		if (t>50) {
+		if (t > 50) {
 			System.out.println("please select temperature in range from 0 to 50");
-			System.exit(0);      // it will exit if it is >50
+			System.exit(0); // it will exit if it is >50
 		}
 		System.out.println("enter velocity : ");
 		double v = s.nextDouble();
-		if (v<3 && v>120) {
+		if (v < 3 && v > 120) {
 			System.out.println("please select velocity in range fronm 3 to 120");
-			System.exit(0);  // used exit commad
+			System.exit(0); // used exit commad
 		}
 		s.close();
-		double w = 35.74+(t*0.615)+(0.4275*t-35.75)*(Math.pow(v, 0.16));  /// Used Math.pow() function.     
-		System.out.printf("The wind chill is :%.2f ",w);                 ///Ex: Math.pow(a, b) where 'a' is base and 'b' is exponent.
+		windChill(t, v);
+	}
 
+	public static void windChill(double t, double v) {
+		double w = 35.74 + (t * 0.615) + (0.4275 * t - 35.75) * (Math.pow(v, 0.16)); /// Used Math.pow() function.
+		System.out.printf("The wind chill is :%.2f ", w); /// Ex: Math.pow(a, b) where 'a' is base and 'b' is exponent.
 
 	}
 
